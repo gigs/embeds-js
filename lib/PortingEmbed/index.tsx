e73f1a6e4acc4f77fbf044190613f7b04417824b
Timo Mämecke <timo@gigs.com>
2024-02-20 14:11:53 +0100

    Fix missing jsdoc annotations

--- lib/PortingEmbed/index.tsx
+++ lib/PortingEmbed/index.tsx
@@ -10,17 +10,37 @@ import {
   PortingEmbed as PortingEmbedComponent,
 } from './PortingEmbed'
 
-type PortingEmbedInit = { project: string }
+type PortingEmbedInit = {
+  /** The ID of your Gigs project. */
+  project: string
+}
 export type PortingEmbedOptions = CustomizableEmbedProps
 
 type Events = never
 
+/**
+ * Initializes an embed to complete a porting (port-in a number). Requires an
+ * authenticated ConnectSession. After initialization, the embed can be mounted
+ * into your document.
+ *
+ * @example
+ * const embed = await PortingEmbed(connectSession, { project: 'my-project' })
+ * // hide your loading states
+ * embed.mount('#embed')
+ *
+ * @param connectSession An authenticated ConnectSession with an intent type of
+ * "completePorting".
+ * @param options Initialization options.
+ */
 export async function PortingEmbed(
   initConnectSession: unknown,
   {
     options: initialOptions,
     project,
-  }: { options?: PortingEmbedOptions } & PortingEmbedInit,
+  }: {
+    /** Additional options to configure the behavior of the embed. */
+    options?: PortingEmbedOptions
+  } & PortingEmbedInit,
 ) {
   // Ensure embed was initialized with proper options.
   assert(
@@ -56,44 +76,6 @@ export async function PortingEmbed(
     `UNSUPPORTED: Porting status "${porting.status}" is not supported by the embed.`,
   )
 
-  /**
-   * Mount the embed into a container.
-   * @param container The HTML Element or selector in which the embed should be
-   * mounted to.
-   */
-  const mount = (container: Element | string) => {
-    assert(container, 'Cannot call mount() without specifying a container.')
-
-    element =
-      typeof container === 'string'
-        ? document.querySelector(container)
-        : container
-    assert(element, 'Element to mount to could not be found.')
-
-    renderWithCurrentOptions()
-  }
-
-  /**
-   * Unmount the mounted embed.
-   */
-  const unmount = () => {
-    assert(element, 'Cannot call unmount() on an unmounted embed.')
-
-    render(null, element)
-    element = null
-  }
-
-  /**
-   * Update the mounted embed with new options.
-   * @param newOptions New options for the embed
-   */
-  const update = (newOptions: PortingEmbedOptions) => {
-    assert(element, 'Cannot call update() on an unmounted embed.')
-
-    options = newOptions
-    renderWithCurrentOptions()
-  }
-
   const renderWithCurrentOptions = () => {
     assert(element, 'No element present to render embed into.')
 
@@ -108,10 +90,62 @@ export async function PortingEmbed(
   }
 
   return {
-    mount,
-    update,
-    unmount,
+    /**
+     * Mount the embed into a container.
+     *
+     * @example
+     * embed.mount('#embed')
+     *
+     * @example
+     * embed.mount(document.getElementById('embed'))
+     *
+     * @param container The HTML Element or selector in which the embed should be
+     * mounted to.
+     */
+    mount(container: Element | string) {
+      assert(container, 'Cannot call mount() without specifying a container.')
+
+      element =
+        typeof container === 'string'
+          ? document.querySelector(container)
+          : container
+      assert(element, 'Element to mount to could not be found.')
+
+      renderWithCurrentOptions()
+    },
+
+    /**
+     * Update the mounted embed with new options.
+     *
+     * @example
+     * embed.update({ styleOptions: { ... }})
+     *
+     * @param newOptions New options for the embed
+     */
+    update(newOptions: PortingEmbedOptions) {
+      assert(element, 'Cannot call update() on an unmounted embed.')
+
+      options = newOptions
+      renderWithCurrentOptions()
+    },
+
+    /**
+     * Unmount the mounted embed.
+     *
+     * @example
+     * embed.unmount()
+     */
+    unmount() {
+      assert(element, 'Cannot call unmount() on an unmounted embed.')
+
+      render(null, element)
+      element = null
+    },
+
+    /** Add an event listener. */
     on: emitter.on.bind(emitter),
+
+    /** Remove event listener. */
     off: emitter.off.bind(emitter),
   }
 }
